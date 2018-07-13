870f81683de3b77a2388830667fe59afbe1cdd4f
bogdanmedvedev <bogdan@medve-dev.net>
2018-07-13 17:18:51 +0300

    fix

--- example.js
+++ example.js
@@ -1,13 +1,15 @@
 const Paypal = require('./index');
+
 const PaypalMerchant = new Paypal.Merchant({
-    mode: "sandbox",
-    client_id: '__ClientId',
-    client_secret: '__Secret',
+    debug: true,
+    username: 'www.clsa.ru_api1.gmail.com',
+    password: 'SEM4MA6YSZZWSNMK',
+    signature: 'Asub74GJyIbzNneLNi-vNmz9Xg.gAyGgGsXkY1V7ifpZV-A8UN2dVtB4',
     callback: {
         error: 'https://error.com',
         success: 'https://success.com'
     }
-}, {debug: false=});
+});
 
 PaypalMerchant
     .getOrderId({
